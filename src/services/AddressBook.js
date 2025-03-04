58279e82032632d505970e76f658dd73613240c6
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-04 15:34:51 +0530

    [Om] Add. prevented adding duplicate contact

--- src/services/AddressBook.js
+++ src/services/AddressBook.js
@@ -1,3 +1,4 @@
+
 const Contact = require('../models/Contact');
 
 class AddressBook {
@@ -6,9 +7,15 @@ class AddressBook {
     }
 
     addContact(contact) {
-        if (this.contacts.some(c => c.phone === contact.phone || c.email === contact.email)) {
-            throw new Error("Duplicate Contact! Phone number or Email already exists.");
+        const isDuplicate = this.contacts.filter(c => 
+            c.firstName.toLowerCase() === contact.firstName.toLowerCase() &&
+            c.lastName.toLowerCase() === contact.lastName.toLowerCase()
+        ).length > 0;
+
+        if (isDuplicate) {
+            throw new Error("Duplicate Contact! A person with the same name already exists.");
         }
+
         this.contacts.push(contact);
         return "Contact added successfully!";
     }

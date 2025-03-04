0ad2040c8cbb05f971e8d649fd5fc326d74878c4
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-04 15:04:06 +0530

    [Om] Add. completed finding and modifying contact

--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Ram", "Patel", "Ward no.27", "Betul", "Madhya pradesh", "490095", "8799467890", "helloworld@gmail.com"
+        "Om", "Tamrakar", "Bhopal", "India", "In", "3245", "1234567890", "helloworld@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Rakesh", "Patel", "Indore", "Satna","Madhya pradesh","450098","8991188633","helloworldd@gmail.com"
+        "Ramu", "Patel", "Bhopal", "India","In","1000000","9198563558","chandalala@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "Man", "cat", "abc", "In", "I", "767A5", "12345", "invalid-email"
+        "man", "pat", "abc", "Ind", "I", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
     console.error("Error:", error.message);
 }
-
 
 //demostrating duplicate contact
 try {

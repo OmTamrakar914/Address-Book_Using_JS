61b30c12ddb5e956379a5df4f53336605494813e
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-04 13:41:00 +0530

    [Om] Add. Validated input contacts

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ const Contact = require('./models/Contact');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Om", "Tamrakar", "Bhopal", "India", "In", "3245", "99464567890", "helloworld@gmail.com"
-);
+try {
+    const contact1 = new Contact(
+        "Ram", "Tamrakar", "ward no.27", "Chhindwara", "Madhya pradesh", "480001", "6847347890", "helloworld@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1));
+    console.log(addressBook.getAllContacts());
+} catch (error) {
+    console.error("Error:", error.message);
+}
 
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const invalidContact = new Contact(
+        "Anush", "Tenguriya", "M", "M", "India", "462921", "9073674834", "helloworld@world.com"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
